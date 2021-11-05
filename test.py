30b0de6d0c4b49abe591034a829fec8388e73bfc
[Lampart] Phan Dai <phan_dai@lampart-vn.com>
2021-11-05 17:13:12 +0700

    [Phase2] stick with Word for 19/11

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
-class A:
-    REWARD = 0
-    def __init__(self, reward) :
-        self.REWARD = reward
-        print('REWARD = ',self.REWARD)
+# Đi từ 1. - 8. trong mô hình nguyên lý hoạt động
+# 1, 2, 7, 8. Tạo form để nhập record hoặc import file .txt, sau đó gửi lên Database ( Quan trọng )
+# > Database dưới dạng Blockchain sẽ lưu như thế nào ? - Blocks
+# > Một Block như vậy thì sẽ lưu gì để đáp ứng nhu cầu biến hóa của Transaction ? - JSON
+# > Khi tạo một Block thì sẽ có các bước gì 
+# FUNC : INSERT, SELECT, BACKUP
 
-    def calc(self, multi = REWARD) :
-        print('MULTI = ',multi)
-        return self.REWARD*multi
-class B:
-    a = A(10)
+RECORD_FORMAT = {
+    'domain' : 'example.com', # Tên miền
+    'type'   : 'A',           # Loại record
+    'value'  : '192.0.2.1',   # Giá trị IP
+    'ttl'    : 14400          # Thời gian tồn tại
+}
 
-b = B()
-print( b.a.calc() ) 
+
+
